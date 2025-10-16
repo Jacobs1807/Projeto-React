aaa2584435454283ab60d93e32b9444aea217a36
Jacobs1807 <gabrieljacobsgarcia@gmail.com>
2025-10-16 12:17:54 -0300

    Add files via upload

--- Back-End/sql.js
+++ Back-End/sql.js
@@ -5,3 +5,7 @@ dotenv.config();
 export const sql = postgre(process.env.DATABASE_URL, {
     ssl: 'require'
 });
+
+
+//DATABASE_URL=DATABASE_URL='postgresql://neondb_owner:npg_RsvQ6hgcS3WX@ep-nameless-base-acmdgmlr-pooler.sa-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'
+//JWT_SECRET=suaChaveUltraSecrete
