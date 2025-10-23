af1b87a686060282de0456a0b0cad7d6c0806e1f
Jacobs1807 <gabrieljacobsgarcia@gmail.com>
2025-10-23 08:51:00 -0300

    Add files via upload

---
+++ frontend/src/Componentes/template/Footer.jsx
@@ -0,0 +1,12 @@
+import './Footer.css';
+import React from 'react';
+
+const Footer = () => (
+    <footer className='footer'>
+        <span>
+            Desenvolvido por Gabe 
+        </span>
+    </footer>
+);
+
+export default Footer;
